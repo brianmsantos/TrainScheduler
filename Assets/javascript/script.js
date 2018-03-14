8b4ce40c4fe5e4228adc4229bc8d2825de838fcf
Brian Santos <brianmsantos81@gmail.com>
2018-03-13 22:00:45 -0700

    firebase good to go

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -19,5 +19,41 @@ let minutes = ""
 $("#submit").on("click", function() {
     event.preventDefault();
     console.log("does this work")
+    train = $("#inputTrainName").val().trim();
+    destination = $("#inputDestination").val().trim();
+    arrival = $("#inputTrainTime").val().trim();
+    minutes = $("#inputFrequency").val().trim();
+
+    database.ref().push({
+        train: train,
+        destination: destination,
+        arrival: arrival,
+        minutes: minutes,
+    })
 
 })
+
+database.ref().on("child_added", function(snapshot) {
+
+    // Log everything that's coming out of snapshot
+
+    console.log(snapshot.val());
+    console.log(snapshot.val().train);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().arrival);
+    console.log(snapshot.val().minutes);
+
+    // Change the HTML to reflect
+
+    $("#inputTrainName").text(snapshot.val().train);
+    $("#inputDestination").text(snapshot.val().destination);
+    $("#inputTrainTime").text(snapshot.val().arrival);
+    $("#inputFrequency").text(snapshot.val().minutes);
+
+    // Handle the errors
+
+}, function(errorObject) {
+
+    console.log("Errors handled: " + errorObject.code);
+
+});
